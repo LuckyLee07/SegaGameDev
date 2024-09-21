59696f590c20fc582f39b9fecbfe2d657d487619
LuckyLee07 <071427li@163.com>
2024-09-21 15:23:00 +0800

    [dev]面向对象重构

--- 2DGraphics1/NimotsuKun/main.cpp
+++ 2DGraphics1/NimotsuKun/main.cpp
@@ -1,79 +1,188 @@
 #include <iostream>
+#include <fstream>
+#include <windows.h>
+#undef max //conflict
+#include <algorithm>
 #include "GameLib/Framework.h"
 
-enum Object
+char* readFile(const char* fileName, int& fileSize)
 {
-	OBJ_SPACE,
-	OBJ_WALL,
-	OBJ_GOAL,
-	OBJ_BLOCK,
-	OBJ_BLOCK_ON_GOAL,
-	OBJ_MAN,
-	OBJ_MAN_ON_GOAL,
+	char cwd[MAX_PATH]; //è·å–å½“å‰å·¥ä½œç›®å½•
+	if (GetCurrentDirectoryA(MAX_PATH, cwd)) {
+		std::cout << "Current working directory: " << cwd << std::endl;
+	}
+	std::ifstream inputFile(fileName, std::ifstream::binary);
+	inputFile.seekg(0, std::ifstream::end);
+	fileSize = static_cast<int>(inputFile.tellg());
+	if (fileSize <= 0) return NULL;
+	
+	inputFile.seekg(0, std::ifstream::beg);
+	char* fileImage = new char[fileSize];
 
-	OBJ_UNKNOWN,
+	if (!inputFile.read(fileImage, fileSize))
+	{
+		delete[] fileImage;
+		return NULL;
+	}
+	return fileImage;
+}
+
+class IntArray2D
+{
+public:
+	IntArray2D() :m_data(NULL), 
+		m_rows(0), m_cols(0)
+	{
+	}
+	IntArray2D(int rows, int cols) :
+		m_rows(rows), m_cols(cols)
+	{
+		m_data = new int[rows * cols];
+	}
+	~IntArray2D()
+	{
+		delete[] m_data;
+		m_data = NULL;
+	}
+	void init(int rows, int cols)
+	{
+		m_rows = rows;
+		m_cols = cols;
+		m_data = new int[rows * cols];
+	}
+	int& operator()(int row, int col)
+	{
+		return m_data[row * m_cols + col];
+	}
+	const int& operator()(int row, int col) const
+	{
+		return m_data[row * m_cols + col];
+	}
+private:
+	int* m_data;
+	int m_rows; //è¡Œ
+	int m_cols; //åˆ—
 };
-const char gStageData[] = "\
-########\n\
-# .. p #\n\
-# oo   #\n\
-#      #\n\
-########";
-const int gStateWidth = 8;
-const int gStateHeight = 5;
 
-void initialize(Object* state, int w, int h, const char* stageData);
-void draw(const Object* state, int w, int h);
-void update(Object* state, char input, int w, int h);
-bool checkClear(const Object* state, int w, int h);
-
-void initialize(Object* state, int w, int h, const char* stageData)
+class GameObj
 {
+public:
+	GameObj(const char* stageData, int dataSize);
+
+	void draw();
+	void update(char input);
+	bool checkClear();
+
+private:
+	void init(const char* stageData, int dataSize);
+
+private:
+	enum Object
+	{
+		OBJ_SPACE,
+		OBJ_WALL,
+		OBJ_GOAL,
+		OBJ_BLOCK,
+		OBJ_BLOCK_ON_GOAL,
+		OBJ_MAN,
+		OBJ_MAN_ON_GOAL,
+
+		OBJ_UNKNOWN,
+	};
+	
+	int m_width;
+	int m_height;
+	IntArray2D m_states;
+};
+
+GameObj::GameObj(const char* stageData, int dataSize)
+{
+	init(stageData, dataSize);
+	m_states.init(m_width, m_height);
+
+	int index = 0;
 	int x = 0, y = 0;
 	const char* d = stageData;
-	while (*d != '\0')
+	while (index < dataSize)
 	{
-		Object t;
-		switch (*d)
+		Object s;
+		switch (d[index])
 		{
-		case '#': t = OBJ_WALL; break;
-		case ' ': t = OBJ_SPACE; break;
-		case 'o': t = OBJ_BLOCK; break;
-		case 'O': t = OBJ_BLOCK_ON_GOAL; break;
-		case '.': t = OBJ_GOAL; break;
-		case 'p': t = OBJ_MAN; break;
-		case 'P': t = OBJ_MAN_ON_GOAL; break;
-		case '\n': //µ½ÏÂÒ»ĞĞ
+		case '#': s = OBJ_WALL; break;
+		case ' ': s = OBJ_SPACE; break;
+		case 'o': s = OBJ_BLOCK; break;
+		case 'O': s = OBJ_BLOCK_ON_GOAL; break;
+		case '.': s = OBJ_GOAL; break;
+		case 'p': s = OBJ_MAN; break;
+		case 'P': s = OBJ_MAN_ON_GOAL; break;
+		case '\n': //åˆ°ä¸‹ä¸€è¡Œ
 			x = 0;
 			++y;
-			t = OBJ_UNKNOWN;
+			s = OBJ_UNKNOWN;
 			break;
-		default: t = OBJ_UNKNOWN; break;
+		default: s = OBJ_UNKNOWN; break;
 		}
-		++d;
-		if (t != OBJ_UNKNOWN)
+		++index;
+		if (s != OBJ_UNKNOWN)
 		{
-			state[y * w + x] = t;
+			m_states(x, y) = s;
 			++x;
 		}
 	}
 }
 
-void draw(const Object* state, int w, int h)
+void GameObj::init(const char* stageData, int dataSize)
+{
+	int width = 0;
+	int height = 0;
+
+	int index = 0;
+	int x = 0, y = 0;
+	const char* d = stageData;
+	while (index < dataSize)
+	{
+		Object s;
+		switch (d[index])
+		{
+		case '#':
+		case ' ':
+		case 'o':
+		case 'O':
+		case '.':
+		case 'p':
+		case 'P':
+			++x; break;
+		case '\n': //åˆ°ä¸‹ä¸€è¡Œ
+			++y;
+			width = std::max(width, x);
+			height = std::max(height, y);
+			x = 0;
+			break;
+		}
+		++index;
+	}
+	//æœ€åä¸€è¡Œæ²¡æ¢è¡Œç¬¦
+	if (x > 0) height = height + 1;
+	
+	m_width = width;
+	m_height = height;
+}
+
+void GameObj::draw()
 {
 	const char font[] = { ' ', '#', '.', 'o', 'O', 'p', 'P' };
-	for (int y = 0; y < h; ++y)
+	for (int y = 0; y < m_height; ++y)
 	{
-		for (int x = 0; x < w; ++x)
+		for (int x = 0; x < m_width; ++x)
 		{
-			Object o = state[y * w + x];
+			int o = m_states(x, y);
 			std::cout << font[o];
 		}
 		std::cout << std::endl;
 	}
 }
 
-void update(Object* state, char input, int w, int h)
+void GameObj::update(char input)
 {
 	int dx = 0, dy = 0;
 	switch (input)
@@ -83,58 +192,66 @@ void update(Object* state, char input, int w, int h)
 	case 'w': dy = -1; break;
 	case 'z': dy = 1; break;
 	}
-	// ²éÕÒÍæ¼ÒÎ»ÖÃ
-	int i = -1;
-	for (i = 0; i < w * h; ++i)
+	
+	int x, y; // æŸ¥æ‰¾ç©å®¶ä½ç½®
+	bool found = false;
+	for (y = 0; y < m_height; ++y)
 	{
-		if (state[i] == OBJ_MAN || state[i] == OBJ_MAN_ON_GOAL)
+		for (x = 0; x < m_width; ++x)
 		{
-			break;
+			int state = m_states(x, y);
+			if (state == OBJ_MAN || state == OBJ_MAN_ON_GOAL)
+			{
+				found = true;
+				break;
+			}
 		}
+		if (found) break;
 	}
 
-	int x = i % w;
-	int y = i / w;
-
-	// ÒÆ¶¯ºóµÄÎ»ÖÃ
+	// ç§»åŠ¨åçš„ä½ç½®
 	int tx = x + dx;
 	int ty = y + dy;
-	if (tx < 0 || ty < 0 || tx >= w || ty >= h) 
+	if (tx < 0 || ty < 0 || tx >= m_width || ty >= m_height)
 		return;
 
-	int p = y * w + x; //Íæ¼ÒÎ»ÖÃ
-	int tp = ty * w + tx; // Ä¿±êÎ»ÖÃ
-	if (state[tp] == OBJ_SPACE || state[tp] == OBJ_GOAL)
+	int ps = m_states(x, y);
+	int tps = m_states(tx, ty);
+	if (tps == OBJ_SPACE || tps == OBJ_GOAL)
 	{
-		state[tp] = state[tp] == OBJ_GOAL ? OBJ_MAN_ON_GOAL : OBJ_MAN;
-		state[p] = state[p] == OBJ_MAN_ON_GOAL ? OBJ_GOAL : OBJ_SPACE;
+		m_states(tx, ty) = tps == OBJ_GOAL ? OBJ_MAN_ON_GOAL : OBJ_MAN;
+		m_states(x, y) = ps == OBJ_MAN_ON_GOAL ? OBJ_GOAL : OBJ_SPACE;
 	}
-	else if (state[tp] == OBJ_BLOCK || state[tp] == OBJ_BLOCK_ON_GOAL)
+	else if (tps == OBJ_BLOCK || tps == OBJ_BLOCK_ON_GOAL)
 	{
-		// ¼ì²âÑØ¸Ã·½ÏòµÄµÚ¶ş¸öÍø¸ñÎ»ÖÃÊÇ·ñÔÚÔÊĞí·¶Î§ÄÚ
+		// æ£€æµ‹æ²¿è¯¥æ–¹å‘çš„ç¬¬äºŒä¸ªç½‘æ ¼ä½ç½®æ˜¯å¦åœ¨å…è®¸èŒƒå›´å†…
 		int tx2 = tx + dx;
 		int ty2 = ty + dy;
 
-		if (tx2 < 0 || ty2 < 0 || tx2 >= w || ty2 >= h) 
+		if (tx2 < 0 || ty2 < 0 || tx2 >= m_width || ty2 >= m_height)
 			return;
 
-		int tp2 = ty2 * w + tx2; //ÑØ¸Ã·½ÏòµÚ¶ş¸öÍø¸ñÎ»ÖÃ
-		if (state[tp2] == OBJ_SPACE || state[tp2] == OBJ_GOAL)
+		int tp2s = m_states(tx2, ty2); //æ²¿è¯¥æ–¹å‘ç¬¬äºŒä¸ªç½‘æ ¼ä½ç½®
+		if (tp2s == OBJ_SPACE || tp2s == OBJ_GOAL)
 		{
-			state[tp2] = state[tp2] == OBJ_GOAL ? OBJ_BLOCK_ON_GOAL : OBJ_BLOCK;
-			state[tp] = state[tp] == OBJ_BLOCK_ON_GOAL ? OBJ_MAN_ON_GOAL : OBJ_MAN;
-			state[p] = state[p] == OBJ_MAN_ON_GOAL ? OBJ_GOAL : OBJ_SPACE;
+			m_states(tx2, ty2) = tp2s == OBJ_GOAL ? OBJ_BLOCK_ON_GOAL : OBJ_BLOCK;
+			m_states(tx, ty) = tps == OBJ_BLOCK_ON_GOAL ? OBJ_MAN_ON_GOAL : OBJ_MAN;
+			m_states(x, y) = ps == OBJ_MAN_ON_GOAL ? OBJ_GOAL : OBJ_SPACE;
 		}
 	}
 }
 
-bool checkClear(const Object* state, int w, int h)
+bool GameObj::checkClear()
 {
-	for (int i = 0; i < w * h; ++i)
+	for (int y = 0; y < m_height; ++y)
 	{
-		if (state[i] == OBJ_BLOCK)
+		for (int x = 0; x < m_width; ++x)
 		{
-			return false;
+			int o = m_states(x, y);
+			if (OBJ_BLOCK == o)
+			{
+				return false;
+			}
 		}
 	}
 	return true;
@@ -142,18 +259,30 @@ bool checkClear(const Object* state, int w, int h)
 
 namespace GameLib {
 	void Framework::update() {
+		unsigned* vram = videoMemory();
+		int iwidth = width();
+		for (int i = 100; i <= 200; ++i)
+		{
+			for (int j = 0; j <= 200; ++j)
+			{
+				vram[j * iwidth + i] = 0xff0000;
+			}
+		}
 	}
 }
 
 int main()
 {
-	Object* state = new Object[gStateWidth * gStateHeight];
-	initialize(state, gStateWidth, gStateHeight, gStageData);
+	int filesize = 0;
+	const char* filepath = "stateData.txt";
+	const char* data = readFile(filepath, filesize);
+
+	GameObj* gState = new GameObj(data, filesize);
 
 	while (true)
 	{
-		draw(state, gStateWidth, gStateHeight);
-		if (checkClear(state, gStateWidth, gStateHeight))
+		gState->draw();
+		if (gState->checkClear())
 		{
 			break;
 		}
@@ -161,12 +290,12 @@ int main()
 		char input;
 		std::cin >> input;
 
-		update(state, input, gStateWidth, gStateHeight);
+		gState->update(input);
 	}
 
 	std::cout << "Congraturation's! you win." << std::endl;
-	delete[] state;
-	state = 0;
+	delete gState;
+	gState = NULL;
 
 	return 0;
 }
